f7fcd3c589e166ca8c19fa27fc636e3a57975e48
SuperMaxine <2586540653@qq.com>
2021-12-26 23:07:00 +0800

    更新测试脚本

---
+++ src/com/company/Test.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.regex.Pattern;
+
+public class Test {
+    public static void main(String[] args) {
+        String regex = "^(a?a)+$";
+        for (int i = 0; i < 100; i++) {
+            String text = "";
+            for (int j = 0; j < i; j++) {
+                text += "a";
+            }
+            text += "!";
+            // print run Time
+            long startTime = System.currentTimeMillis();
+            System.out.println(i);
+            System.out.println(Pattern.matches(regex, text));
+            long endTime = System.currentTimeMillis();
+            System.out.println("Run Time: " + (endTime - startTime) + "ms");
+        }
+    }
+}
